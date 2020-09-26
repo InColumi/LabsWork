36921838efd1cb425995e34bab3570411b182d59
Maxim <max77648@gmail.com>
2020-09-26 15:32:55 +0300

    Сделал первую лабу

--- LabsWork/main.cpp
+++ LabsWork/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <stdio.h>
-#include <string>
+#include <stdlib.h> // для перехода на русский язык
 #include <ctime>
-
+#include <string>
+#include <windows.h>
 using namespace std;
 
 struct Store
 {
 	public:
-	string Name;
+	char Name[17];
 	int CountArtBooks;
 	int CountSportBooks;
 	int CountEducationBooks;
@@ -16,59 +17,63 @@ struct Store
 
 	void ShowNames()
 	{
-		cout << "Название магазина" << "\t" <<
-			"Художественые" << "\t" <<
-			"Спортивные" << "\t" <<
-			"Учебные " << "\t" <<
-			"Развлекательные\n";
+		printf("%-0.17s\t%-0.17s\t%-0.17s\t%-0.17s\t%-0.17s\n", "Название магазина", "Художественые", "Спортивные", "Обучающие", "Развлекательные");
 	}
 
 	void ShowValueInfo()
 	{
-		cout << '|'  <<
-			Name << "\t\t" <<
-			CountArtBooks << "\t\t" <<
-			CountSportBooks << "\t\t" <<
-			CountEducationBooks << "\t\t" <<
-			CountEntertainingBooks << "\t\n";
-		
-		/*cout << '|' <<
-			Name << setw(25) << left <<
-			CountArtBooks << setw(22) <<
-			CountSportBooks << setw(22) <<
-			CountEducationBooks << setw(15) <<
-			CountEntertainingBooks << setw(15) << "|\n";*/
+		printf("%-17s\t%-13d\t%-10d\t%-9d\t%-17d\n", Name, CountArtBooks, CountSportBooks, CountEducationBooks, CountEntertainingBooks);
+	}
+
+	void InputFromConsole()
+	{
+		cout << "Введите название книжного магазина: ";
+		cin >> Name;
+		cout << "Введите кол-во проданных художественных книг: ";
+		cin >> CountArtBooks;
+		cout << "Введите кол-во проданных спортивных книг: ";
+		cin >> CountSportBooks;
+		cout << "Введите кол-во проданных учебных книг: ";
+		cin >> CountEducationBooks;
+		cout << "Введите кол-во проданных развлекательных книг: ";
+		cin >> CountEntertainingBooks;
+	}
+
+	void SetValue(int artBooks, int sportBooks, int educationBooks, int entertainingBooks)
+	{
+		CountArtBooks = artBooks;
+		CountSportBooks = sportBooks;
+		CountEducationBooks = educationBooks;
+		CountEntertainingBooks = entertainingBooks;
 	}
 };
 
 Store* GenerateStores(int countSores)
 {
+	char* names = *new char[6][12]{ 
+		"Свобода", 
+		"Надежда", 
+		"Улыбка", 
+		"Свобода",
+		"Радуга", 
+		"Закат" };
+	int randMaxNumber = 10;
 	Store* newStores = new Store[countSores];
 	for(int i = 0; i < countSores; i++)
 	{
-		newStores[i].Name = "name123213123";
-		newStores[i].CountArtBooks = 0 + rand() % 11;
-		newStores[i].CountSportBooks = 0 + rand() % 11;
-		newStores[i].CountEducationBooks = 0 + rand() % 11;
-		newStores[i].CountEntertainingBooks = 0 + rand() % 11;
+		cout << "Имя: ";
+		cin >> newStores[i].Name;
+		newStores[i].CountArtBooks = 0 + rand() % randMaxNumber;
+		newStores[i].CountSportBooks = 0 + rand() % randMaxNumber;
+		newStores[i].CountEducationBooks = 0 + rand() % randMaxNumber;
+		newStores[i].CountEntertainingBooks = 0 + rand() % randMaxNumber;
 	}
 	return newStores;
 }
-void Laba1()
+
+void Generator(int countStores)
 {
 	srand(time(0));
-	Store store;
-	/*cout << "Введите название книжного магазина: ";
-	cin >> store.Name;
-	cout << "Введите кол-во проданных художественных книг: ";
-	cin >> store.CountArtBooks;
-	cout << "Введите кол-во проданных спортивных книг: ";
-	cin >> store.CountSportBooks;
-	cout << "Введите кол-во проданных учебных книг: ";
-	cin >> store.CountEducationBooks;
-	cout << "Введите кол-во проданных развлекательных книг: ";
-	cin >> store.CountEntertainingBooks;*/
-	int countStores = 10;
 	Store* stores = GenerateStores(countStores);
 	stores[0].ShowNames();
 	for(size_t i = 0; i < countStores; i++)
@@ -77,9 +82,20 @@ void Laba1()
 	}
 }
 
+void Laba1()
+{
+	Store store;
+	store.InputFromConsole();
+	store.ShowNames();
+	store.ShowValueInfo();	
+}
+
 int main()
 {
+	system("chcp 1251"); // переходим в консоли на русский язык
+	system("cls");
 	setlocale(LC_ALL, "Russian");
 	Laba1();
+	//Generator(1);
 	return 0;
 }
